5b6fd6cdb0a8c5a9bcfd2ed9e816633166edeaf4
kayliewambui <kayliewambui20@gmail.com>
2024-04-17 11:45:00 +0300

    initial commit -m

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let re = /[\W_]/g;
+    let lowRegStr = word.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
 }
 
 /* 
